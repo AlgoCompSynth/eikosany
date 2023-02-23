b449ef541d30ebe282ccab2e61dd0010d85d8795
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2023-02-22 17:36:31 -0800

    dataset infrastructure

---
+++ data-raw/DATASET.R
@@ -0,0 +1,18 @@
+## code to prepare `DATASET` dataset goes here
+
+edo12_names <- c(
+  "C",
+  "C#|Db",
+  "D",
+  "D#|Eb",
+  "E",
+  "F",
+  "F#|Gb",
+  "G",
+  "G#|Ab",
+  "A",
+  "A#|Bb",
+  "B",
+  "C"
+)
+usethis::use_data(edo12_names, overwrite = TRUE)
