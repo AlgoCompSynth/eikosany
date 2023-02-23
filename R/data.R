e029ab8f25ae2bc7091bc3a9dbdcd52c565b72eb
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2023-02-22 18:39:59 -0800

    doc tweaks

--- R/data.R
+++ R/data.R
@@ -1,10 +1,10 @@
-#' Note name datasets
+#' Note names for 12EDO
 #'
 #' For scale tables created using function `et_scale_table`, the user must
 #' specify note names. As a convenience, the `eikosany` package includes
 #' data sets for `12EDO`, `19EDO`, and `31EDO`.
 #'
-#' About the note names: even in 2023, code often breaks when given
+#' Even in 2023, code often breaks when given
 #' characters outside the 7-bit international standard. So we don't even
 #' have flats, let alone half-flats, half-sharps, naturals, or any of the
 #' other symbols alternate tuning theorists have proposed.
@@ -14,19 +14,48 @@
 #' for a half-sharp, "#" for a sharp. and "##" for a double-sharp. Thanks
 #' in advance for your understanding.
 #'
-#' Note names for 12EDO:
 #' @format ## `edo12_names`
 #' A character vector with 12 elements
 #' @source a music theory class
 "edo12_names"
 #'
-#' Note names for 19EDO:
+#' Note names for 19EDO
+#'
+#' For scale tables created using function `et_scale_table`, the user must
+#' specify note names. As a convenience, the `eikosany` package includes
+#' data sets for `12EDO`, `19EDO`, and `31EDO`.
+#'
+#' Even in 2023, code often breaks when given
+#' characters outside the 7-bit international standard. So we don't even
+#' have flats, let alone half-flats, half-sharps, naturals, or any of the
+#' other symbols alternate tuning theorists have proposed.
+#'
+#' So for scales created with equal divisions of the octave, we use a
+#' "/b" for a half-flat, "b" for a flat, "bb" for a double-flat, /#"
+#' for a half-sharp, "#" for a sharp. and "##" for a double-sharp. Thanks
+#' in advance for your understanding.
+#'
 #' @format ## `edo19_names`
 #' A character vector with 19 elements
 #' @source <https://en.wikipedia.org/wiki/19_equal_temperament#Notation>
 "edo19_names"
 #'
-#' Note names for 31EDO:
+#' Note names for 31EDO
+#'
+#' For scale tables created using function `et_scale_table`, the user must
+#' specify note names. As a convenience, the `eikosany` package includes
+#' data sets for `12EDO`, `19EDO`, and `31EDO`.
+#'
+#' Even in 2023, code often breaks when given
+#' characters outside the 7-bit international standard. So we don't even
+#' have flats, let alone half-flats, half-sharps, naturals, or any of the
+#' other symbols alternate tuning theorists have proposed.
+#'
+#' So for scales created with equal divisions of the octave, we use a
+#' "/b" for a half-flat, "b" for a flat, "bb" for a double-flat, /#"
+#' for a half-sharp, "#" for a sharp. and "##" for a double-sharp. Thanks
+#' in advance for your understanding.
+#'
 #' @format ## `edo31_names`
 #' A character vector with 31 elements
 #' @source <https://en.wikipedia.org/wiki/31_equal_temperament#Scale_diagram>
