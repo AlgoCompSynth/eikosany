90565965ca95a73acf9fc3d3b6bd68ff25003179
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2023-08-16 18:07:50 -0700

    adding chord player

--- R/output_functions.R
+++ R/output_functions.R
@@ -341,33 +341,61 @@ scale_multisample <- function(
 
 }
 
-#' @title Create Chord Sawtooth WAV Files
-#' @name chord_sawtooth_WAVs
-#' @description Creates a `gm` music object for a given chord and keyboard map
-#' @export chord_sawtooth_WAVs
+#' @title Create Chord WAV Files
+#' @name chord_WAVs
+#' @description Creates WAV files for inversions of a chord between
+#' two notes
+#' @export chord_WAVs
 #' @param chord a numeric vector with the scale degrees for the chord
 #' @param keyboard_map the keyboard map for the scale
-#' @param lowest_note the lowest MIDI note number to use. Default is 48.
-#' @param highest_note the highest MIDI note number to use. Default is 84.
+#' @param lowest_note the lowest MIDI note number to use, default
+#' is 48.
+#' @param highest_note the highest MIDI note number to use, default
+#' is 84.
 #' @param output_directory character, default "~/Multisample". This will
 #' be created if it does not exist.
-#' @param duration_sec how long to hold each note, default = 1
+#' @param signal the `seewave` signal type, default is "saw"
+#' @param duration_sec how long to hold each note, default = 4
 #' @param velocity MIDI velocity, default = 100, max is 127
 #' @param sample_rate_hz sample rate in hz, default = 48000
 #' @param bit_width bit width of samples, default = 24
 #' @returns the full path to output_directory
+#' @examples
+#' \dontrun{
+#' eikosany <- cps_scale_table()
+#' eikosany_chords <- cps_chord_table(eikosany)
+#' eikosany_map <- keyboard_map(eikosany)
+#' chord_degrees <- eikosany_chords$degrees
+#' for (i in length(chord_degrees)) {
+#'   chord <- as.numeric(unlist(strsplit(chord_degrees[i], ":")))
+#'   folder_name <-
+#'     paste0("~/eikosany-chords/chord-", gsub(":", "-", chord_degrees[i]))
+#'   print(paste0("generating WAVs in folder ", folder_name))
+#'   chord_WAVs(
+#'     chord,
+#'     keyboard_map = eikosany_map,
+#'     lowest_note = 40,
+#'     highest_note = 80,
+#'     output_directory = folder_name
+#'   )
+#' }
+#' }
 #'
-chord_sawtooth_WAVs <- function(
+chord_WAVs <- function(
     chord,
     keyboard_map,
     lowest_note = 48,
     highest_note = 84,
     output_directory = "~/Multisample",
+    signal = "saw",
     duration_sec = 1,
     velocity = 100,
     sample_rate_hz = 48000,
     bit_width = 24
 ) {
+
+  # create the directory
+  dir.create(output_directory, recursive = TRUE)
 
   # get the frequencies for all the notes in the chord
   chord_map <- keyboard_map[
@@ -376,6 +404,7 @@ chord_sawtooth_WAVs <- function(
       note_number <= highest_note
   ]
   frequencies <- sort(chord_map$freq)
+  note_numbers <- sort(chord_map$note_number)
 
   # generate the chords
   chord_span <- length(chord)
@@ -386,7 +415,32 @@ chord_sawtooth_WAVs <- function(
     end_index <- root_index + chord_span - 1
     if (end_index > last_index) { break }
     chord_frequencies <- frequencies[root_index:end_index]
-    print(chord_frequencies)
+    chord_label <- paste(
+      note_numbers[root_index:end_index],
+      collapse = "_"
+    )
+    wave_object <- chord_synth(
+      chord_frequencies,
+      signal = signal,
+      duration_sec = 4,
+      velocity = 100,
+      sample_rate_hz = 48000,
+      bit_width = 24
+    )
+
+    # make file name -
+    file_name <- sprintf(
+      "chord-%s-%s.wav",
+      signal,
+      chord_label
+    )
+    file_path <- paste0(output_directory, "/", file_name)
+
+    # and write it!
+    summary(wave_object)
+    print(file_path)
+    tuneR::writeWave(wave_object, file_path)
+
     root_index <- root_index + 1
   }
 
